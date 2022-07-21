bf9896d6d1030cbfc519d93f6b323f3f8dfe9f82
boham97 <kangbosung97@gmail.com>
2022-07-21 16:56:14 +0900

    0721_class_end

---
+++ 14362.py
@@ -0,0 +1,25 @@
+test_case = int(input())
+
+for test in range(test_case):
+    arr = list(input())
+    robot = [0, 0]
+    angles = [[1,0],[0,1],[-1,0],[0,-1]]
+    angle = 0
+
+    for i in arr:
+        if i == 'L':
+            angle += 1
+            if angle == 4:
+                angle = 0
+        elif i == 'R':
+            angle -= 1
+            if angle == -1:
+                angle = 3
+        elif i == 'S':
+            robot[0] = robot[0] + angles[angle][0]
+            robot[1] = robot[1] + angles[angle][1]
+    print(robot, angle)
+    if angle == 0 and (robot[0] !=0 or robot[1] != 0):
+        result = 'oo'
+    else: 
+        pass
