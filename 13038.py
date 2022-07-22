5eb4e9f68b4708fb5cd0e490a4137105d706534a
boham97 <kangbosung97@gmail.com>
2022-07-22 15:37:49 +0900

    022class

--- 13038.py
+++ 13038.py
@@ -4,21 +4,17 @@ for z in range(test):
     
     time= int(input())
     week= list(map(int,input().split(' ')))
-    twoweek= week+week
-
-    day, remain= divmod(time,sum(week))
-    day= 7*(day-1)
-    remain+= sum(week)
-
-    minday=14
+    min_day = time * 7
     for i in range(7):
-        temp= 0
-        for j in range(14-i):
-            temp+=twoweek[i+j]
-            if temp== remain:
-                if j<minday:
-                    minday=j
-                break
+        if week[i] == 1:
+            learned = 0
+            day = 0
+            while(learned < time):
+                learned += week[(i + day )%7]
+                day +=1
+                #print(day, learned)
+            if day < min_day:
+                min_day = day
     print('#',end='')
-    print(z+1,end=' ')
-    print(day+minday+1)
+    print(z+1,'',end='')
+    print(min_day)
