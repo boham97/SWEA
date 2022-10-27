5f36e511cb8adee77c96a8b0a94a95fa2a4c25b8
boham97 <kangbosung97@gmail.com>
2022-10-27 17:37:33 +0900

    파이프 연결

---
+++ 4340.py
@@ -0,0 +1,64 @@
+from pprint import pprint
+
+
+def dfs(x, y, dct, n):
+    global cnt
+    if visited[dct][x][y] <= n or cnt <= n or visited[4][x][y] == 1:
+        return
+    if x == N and y == N + 1:
+        if cnt > n:
+            cnt = n
+            for x,y,dc,n in stack:
+                visited[dc][x][y] = n
+        return
+    visited[4][x][y] = 1
+    stack.append([x,y,dct,n])
+    if arr[x][y] == 0:
+        visited[3][x][y] = 1
+        visited[2][x][y] = 1
+        visited[1][x][y] = 1
+        visited[0][x][y] = 1
+    elif arr[x][y] <3:
+        if dct == 0:
+            dfs(x, y+1, dct, n + 1)
+        elif dct == 1:
+            dfs(x + 1, y, dct, n + 1)
+        elif dct == 2:
+            dfs(x, y - 1, dct, n + 1)
+        elif dct == 3:
+            dfs(x - 1, y, dct, n + 1)
+    elif arr[x][y] >2:
+        if dct == 0:
+            dfs(x + 1, y, 1, n + 1)
+            dfs(x - 1, y, 3, n + 1)
+        elif dct == 1:
+            dfs(x, y + 1, 0, n + 1)
+            dfs(x, y - 1, 2, n + 1)
+        elif dct == 2:
+            dfs(x + 1, y, 1, n + 1)
+            dfs(x - 1, y, 3, n + 1)
+        else:
+            dfs(x, y + 1, 0, n + 1)
+            dfs(x, y - 1, 2, n + 1)
+    visited[4][x][y] = 0
+    stack.pop()
+    
+    
+tc = int(input())
+for test in range(tc):
+    N = int(input())
+    cnt = N*N+2
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    visited = [[[cnt] * (N + 2) for _ in range(N + 2)] for _ in range(4)]
+    visited.append([[0]* (N+2) for _ in range(N+2)])
+    arr.append([0] * (N + 2))
+    arr.insert(0, [0] * (N + 2))
+    for i in range(1, N + 1):
+        arr[i].append(0)
+        arr[i].insert(0, 0)
+    arr[1][0] = 1
+    arr[N][N + 1] = 1
+    stack =[]
+    dfs(1, 0, 0, 0)
+    print(f'#{test + 1} {cnt-1}')
+
